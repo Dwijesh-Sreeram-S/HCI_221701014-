905bde929cb0400f919d9cf05591f57a7f0626db
Dwijesh-Sreeram-S <144356474+Dwijesh-Sreeram-S@users.noreply.github.com>
2024-06-08 15:12:08 +0530

    Add files via upload

---
+++ simple calculator/java/com/mycompany/exp1/Exp1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exp1;
+
+/**
+ *
+ * @author student1
+ */
+public class Exp1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
